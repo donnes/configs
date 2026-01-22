d13550232f8c04b6309e6a30b4f4ac9aa764bf18
Donald Silveia <donaldsilveira@gmail.com>
2026-01-22 12:13:02 -0300

    Update dotfiles

---
+++ configs/dotfiles/.config/nvim/init.lua
@@ -0,0 +1,36 @@
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazy_path) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazy_path,
+  })
+end
+
+vim.opt.rtp:prepend(lazy_path)
+
+require("core.options")
+require("core.keymaps")
+require("core.autocmds")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("nvim-tree.api").tree.open()
+  end,
+})
+
+require("lazy").setup("plugins", {
+  ui = {
+    border = "rounded",
+  },
+  change_detection = {
+    notify = false,
+  },
+  rocks = {
+    enabled = false,
+  },
+})
